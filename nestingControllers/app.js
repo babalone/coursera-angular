807a4d7c507479c96f028802a557aaf65d272dfb
stSpielmann <github@bazzinga.eu>
2016-11-14 11:32:42 +0100

    added something to nesting

--- nestingControllers/app.js
+++ nestingControllers/app.js
@@ -10,6 +10,8 @@ function NestedAppParentController($scope, $filter){
   $scope.parentValue = 1;
   $scope.parentController = this;
   $scope.parentController.parentValue = 1;
+  var parent = this;
+  parent.value = 1;
   console.log("parent: $scope", $scope);
 };
 
@@ -20,6 +22,8 @@ console.log("$scope", $scope);
   console.log("$scope.$parent.parentValue", $scope.$parent.parentValue);
   console.log("$scope.parentController", $scope.parentController);
   console.log("$scope.parentController.parentValue", $scope.parentController.parentValue);
+  var child = this;
+  child.value = 6;
   $scope.parentValue = 4;
   $scope.parentController.parentValue = 5;
   console.log("$scope", $scope);
